3ec319c0eada29d35fb1926bc628d2f3a1a84ae7
buych1 <buych1@naver.com>
2024-09-13 13:19:29 +0900

    test2

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ if __name__ == '__main__':
     pn.is_prime_number(31)
     pn.is_prime_number(45)
     pn.is_prime_number(97)
+
+
+    pn.is_prime_number(1)
+    pn.is_prime_number(13)
+    pn.is_prime_number(24)
+    pn.is_prime_number(31)
+    pn.is_prime_number(45)
+    pn.is_prime_number(97)
+
+
+    pn.is_prime_number(1)
+    pn.is_prime_number(13)
+    pn.is_prime_number(24)
+    pn.is_prime_number(31)
+    pn.is_prime_number(45)
+    pn.is_prime_number(97)
